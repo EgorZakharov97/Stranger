65b49ee8b3edcc2c0c1817383783a8b5f01e300b
egorzakharov97 <egor.zakharov97@gmail.com>
2021-09-24 22:38:38 -0500

    Marketplace

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -11,9 +11,80 @@ contract Marketplace is ReentrancyGuard {
     Counters.Counter private _itemIds;
     Counters.Counter private _itemsSold;
 
-    address owner;
+    address payable private _owner;
+
+    struct Listing {
+        uint itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    mapping(address => Listing[]) private _userToItems;
+    mapping(uint256 => Listing) private _idToListing;
+    mapping(address => uint256) private _addressToCustomerCounter;
     
     constructor() {
-        owner = msg.sender;
+        _owner = payable(msg.sender);
     }
+
+    event ItemListed(
+        uint indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    event ItemSold(
+        uint indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    function createListing(
+        address nftContract,
+        uint256 tokenId,
+        uint256 price
+    ) public nonReentrant {
+        require(price >= 0, "Price cannot be negative");
+
+        uint256 newItemId = _itemIds.current();
+        _itemIds.increment();
+
+        // Create new Listing record
+        _idToListing[newItemId] = Listing(
+            newItemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
+        
+        // Increment user counter
+        _addressToCustomerCounter[tx.origin]++;
+        // Push the same item to the user's collection
+        _userToItems[tx.origin].push(_idToListing[newItemId]);
+
+        // Authorize self to move this token
+        ERC721(nftContract).approve(address(this), tokenId);
+
+        emit ItemListed(newItemId, nftContract, tokenId, tx.origin, address(0), price, false);
+    }
+
+    function executePurchase(uint256 listingId) public nonReentrant {
+        
+    }
+
 }
